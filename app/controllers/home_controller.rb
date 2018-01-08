a954cf281638af4908959a60427c6ebd1bd2c98a
Lucas Cândido de Souza Silva <lcssbr@gmail.com>
2018-01-08 07:15:38 -0200

    report page view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,13 @@ require Rails.root.join('app/services/quake_log_parser_service')
 class HomeController < ApplicationController
   def index
   end
+
+  def report
+    if params[:log].blank?
+      flash[:error] = 'The log field is required.'
+      return redirect_to '/'
+    end
+
+    @log_data = QuakeLogParserService.new(params[:log].path).log_data
+  end
 end
